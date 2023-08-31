df4280c0e3d4e2d4d72c0666bc186fc6a332a287
Soeurm Veha <140709495+Veha2023@users.noreply.github.com>
2023-08-31 12:01:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript for interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    // Add a smooth scrolling behavior to internal links
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
+    internalLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+        });
+    });
+});
